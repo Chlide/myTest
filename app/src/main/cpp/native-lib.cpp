8d55b2db134d3f1afea353c29c0718ad54e7db4e
LyGo2333 <150650096+LyGo2333@users.noreply.github.com>
2023-11-13 14:33:13 +0800

    Pertner Update native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,7 +5,7 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_myapplicationtest_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from Worker B";
     return env->NewStringUTF(hello.c_str());
 }
 // Here is an update!
